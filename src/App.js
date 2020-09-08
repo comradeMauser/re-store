39898e9060cad2cd76b905cdc7431c3f9fdcddfc
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-09-08 22:35:57 +0300

    upd

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./app.css";
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>show must go on</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/Error-indicator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorIndicator = () => {
+    return (
+        <div>
+            it's broken!
+        </div>
+    );
+};
+
+export default ErrorIndicator;
