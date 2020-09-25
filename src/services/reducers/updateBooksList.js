f87e74877777f573c6f899298284816aebd0e970
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-09-25 14:27:50 +0300

    upd

--- src/services/reducers/updateBooksList.js
+++ src/services/reducers/updateBooksList.js
@@ -23,4 +23,5 @@ const updateBooksList = (state, action) => {
             return state.booksList
     }
 };
+
 export default updateBooksList
--- src/services/store.js
+++ src/services/store.js
@@ -1,5 +1,20 @@
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import reducer from "./reducer";
 
-const store = createStore(reducer);
+const logMiddleware = () => (next) => (action) => {
+    console.debug(action.type);
+    return next(action)
+};
+
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === "string") {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+};
+
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
+
 export default store;
